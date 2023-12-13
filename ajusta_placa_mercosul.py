fbc3731875415e1850499c930f17f0f78946b6f1
Fabricio Barbosa <62267152+fabriciogeog@users.noreply.github.com>
2023-12-13 12:17:41 -0300

    Adicionado arquivo
    Adicionado

---
+++ ajusta_placa_mercosul.py
@@ -0,0 +1,34 @@
+"""Aplicação para ajustar modelo placas veiculares."""
+
+import re
+import pandas as pd
+
+
+def confere_placa(placa_antiga):
+    padrao = r'^[a-zA-Z]{3}[0-9]{4}$'
+    if re.fullmatch(padrao, placa_antiga) is None:
+        print('Formato da placa inválido!')
+
+    else:
+        print('Formato válido')
+        lista = list(placa_antiga)
+        print('Dígito que será substituído: ', lista[4])
+        tabela = pd.read_csv('equivalencia_placa_mercosul.csv', delimiter=';')
+        equivalente = tabela.iloc[int(lista[4])]
+        lista[4] = equivalente[1]
+        placa_antiga = ''.join(lista)
+        print('Placa atualizada: ', placa_antiga.upper())
+
+
+if __name__ == '__main__':
+    while True:
+        placa_informada = input(str('Informe a placa (NÃO USAR TRAÇO): '))
+        confere_placa(placa_informada)
+        resposta = str.upper(input('Deseja ajustar outra placa (S/N): '))
+        if resposta == 'S':
+            continue
+        elif resposta == 'N':
+            break
+        else:
+            print('Opção inválida!')
+            break
